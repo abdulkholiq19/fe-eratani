ecd1975217dcfcb32898242da32ff38f37e3a487
Abdul Kholik <macbook@Abduls-MacBook-Pro.local>
2022-03-04 16:46:33 +0700

    logic search

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import {
 } from "react-bootstrap";
 
 function App() {
+
+  const data = ['Danawi Liam', 'Tarjaya', 'Daruna', 'Warsoni', 'John Wick', 'Hadi PS', 'Derian Lekso' ]
+  var value = 'Tarjaya';
+  const newData = data.find((str) => str === value);
+  console.log('Value Of Search :', value)
+  console.log('Search Data :', newData)
   return (
     <div>
       <Container>
-        <h1>
-          Logic Search
-        </h1>
+        <h3 className="text-center mt-5">
+          Logic
+        </h3>
+        <h4 className="text-center mt-5 gap-5">
+          {data}
+        </h4>
+        <h4 className="text-center mt-5 gap-5">
+          Search Data :{newData}
+        </h4>
       </Container>
     </div>
   );
