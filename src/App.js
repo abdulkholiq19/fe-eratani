860193872831f68394cdc978d8530d5fe03e7d51
Abdul Kholik <macbook@Abduls-MacBook-Pro.local>
2022-03-04 16:24:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import { NavbarComponent } from './components/NavbarComponent';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar } from "@fortawesome/free-regular-svg-icons";
-import { FooterComponent } from './components/FooterComponent';
 
 import {
-  Navbar,
   Container,
-  Nav,
-  Image,
-  Card,
-  Row,
-  Col,
-  Button
 } from "react-bootstrap";
 
 function App() {
+  const data = [8, 2, 20, -7, 25, -1, 5];
+  data.sort(function(a, b){return a - b});
   return (
     <div>
-      <h3>
-        Logic
-      </h3>
+      <Container>
+        <h3 className="text-center mt-5">
+          Logic
+        </h3>
+        <h4 className="text-center mt-5 gap-5">
+          {data}
+        </h4>
+      </Container>
     </div>
   );
 }
